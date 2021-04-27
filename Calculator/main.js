6ef3de7e02597a7cdef97753463e6bac66f266b2
realjacobryer <jacob.schermerhorn53@journalism.cuny.edu>
2021-04-27 16:37:07 -0400

    updating calculator section

---
+++ Calculator/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+
+	const item_rows = document.querySelectorAll(".calculator .row");
+
+	item_rows.forEach(function (row) {
+		const select = row.querySelector("input");
+		select.addEventListener("change", item_input_listener)
+	});
+});
+
+function item_input_listener(e) {
+	const this_input = e.target;
+	const row = this_input.closest(".row");
+
+	row.classList.add("active");
+	calculate_totals();
+}
